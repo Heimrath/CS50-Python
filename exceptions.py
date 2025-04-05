b8667825c8db34caf10af00433e9a2d8637d385b
Matheus Heimrath <129696259+Heimrath@users.noreply.github.com>
2025-04-05 11:21:36 -0300

    Create exceptions.py

---
+++ exceptions.py
@@ -0,0 +1,25 @@
+def main():
+    x = obter_inteiro("Qual é o valor de y? ")
+    print(f"x = {x} \n")
+
+
+def obter_inteiro(mensagem):
+    while True:
+        try:
+            return int(input("\n" + mensagem))
+            # return valor
+        except ValueError:
+            print("Valor inválido. Por favor, insira um número inteiro.")
+
+main()
+
+while True:    
+    try:
+        opcao = int(input("Escolha uma opção:\n1 - Adição\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n0 - Sair\nOpção: "))
+    except ValueError:
+        print("Opção inválida. Por favor, insira um número inteiro.")
+    else:
+        if 0 <= opcao <= 4:
+            print(f"Você escolheu a opção: {opcao}\n")
+            if opcao == 0:
+                break
